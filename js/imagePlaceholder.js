515c624da6259135612c4d1179a7a15f7c954d0c
Cate Madden <158115037+ccmadden@users.noreply.github.com>
2024-11-04 15:34:47 -0500

    Add files via upload

---
+++ js/imagePlaceholder.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('img').forEach(img => {
+    console.log("runs")
+    img.onerror = function() {
+      this.onerror = null; // Prevents infinite loop if default image missing
+      this.src = '../js/placeholder-image.jpg';
+      this.alt = ""
+    };
+  });
+  
